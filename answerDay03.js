4ce2fe46a9b3a8873761796799b28994ee672b5d
Duc Tai TRAN <tdts2010@gmail.com>
2024-12-06 00:24:02 +0100

    AOC2015

--- answerDay03.js
+++ answerDay03.js
@@ -16,7 +16,7 @@ const visitedHouses = [...data].reduce(([house, x, y], value) => {
   return [house, x, y];
 }, [{ '0:0': 1}, 0, 0]);
 
-console.log('answer01:', Object.keys(visitedHouses[0]).length)
+console.log('answer01:', Object.keys(visitedHouses[0]).length);
 
 const nextVisitedHouses = [...data].reduce(([house, santaPos, roboPos], value, index) => {
   const isSantaTurn = index % 2 === 0;
@@ -29,10 +29,5 @@ const nextVisitedHouses = [...data].reduce(([house, santaPos, roboPos], value, i
 }, [{ '0,0': 2 }, [0, 0], [0, 0]]);
 
 console.log('answer02:', Object.keys(nextVisitedHouses[0]).length);
-
-
-
-
-
 
 console.timeEnd('Execution Time');
